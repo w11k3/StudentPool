f58550d4db89daead66c1ad2ece2f7f2cdfa6501
Paulo Kinjo <paulokinjo@hotmail.com>
2024-07-05 18:14:41 -0400

    making a exception handling in array

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿public class StudentPool
+{
+    public static void Main(string[] args)
+    {
+        // student response array (more typically, input at run time)
+        int[] responses = { 1, 2, 5, 4, 3, 5, 2, 1, 3, 3, 1, 4, 3, 3, 3, 2, 3, 3, 2, 14 };
+        int[] frequency = new int[6]; // array of frequency counters
+
+        // for each answer, select responses element and use that value
+        // as frequency index to determine element to increment
+        for (int answer = 0; answer < responses.Length; ++answer)
+        {
+            try
+            {
+                ++frequency[responses[answer]];
+            } // end try
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(" responses({0}) = {1}\n", answer, responses[answer]);
+            } // end catch
+        } // end for 
+
+        Console.WriteLine("{0}{1,10}", "Rating", "Frequency");
+
+        // output each array element's value
+        for (int rating = 1; rating < responses.Length; ++rating)
+            Console.WriteLine("{0,6}{1,10}", rating, frequency[rating]);
+    } // end Main
+} // end class StudentPool
